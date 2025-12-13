390fcb952729a6cb0475ae49b7a42ac2dd21398d
Aditya Gusyanda A <aditgusyandaa@gmail.com>
2025-12-13 15:42:38 +0700

    Update cinematic.js

--- cinematic.js
+++ cinematic.js
@@ -18,3 +18,30 @@ function startTyping() {
         setTimeout(startTyping, 90);
     }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const audio = document.getElementById("bgm");
+    const icon = document.getElementById("soundIcon");
+
+    audio.volume = 0.8;
+
+    // restore sound state
+    if (localStorage.getItem("sound") === "on") {
+        audio.play().catch(() => {});
+        icon.classList.replace("fa-volume-xmark", "fa-volume-high");
+    }
+
+    window.toggleSound = function () {
+        if (audio.paused) {
+            audio.play().catch(() => {});
+            icon.classList.replace("fa-volume-xmark", "fa-volume-high");
+            localStorage.setItem("sound", "on");
+        } else {
+            audio.pause();
+            icon.classList.replace("fa-volume-high", "fa-volume-xmark");
+            localStorage.setItem("sound", "off");
+        }
+    };
+
+});
